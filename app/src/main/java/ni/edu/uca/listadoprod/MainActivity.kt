c49d7113c575dc3f05c195d38554f456ae912822
ashly31 <105886487+ashly31@users.noreply.github.com>
2022-10-03 19:23:29 -0700

    Update MainActivity.kt

--- app/src/main/java/ni/edu/uca/listadoprod/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/listadoprod/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        iniciar()
+        iniciar() 
     }
 
     private fun limpiar() {
